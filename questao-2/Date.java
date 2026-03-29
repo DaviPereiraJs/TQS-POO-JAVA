d9c8e0e3c8a152b17b08db85f87748b510b00b41
DaviPereiraJs <davi.nascimento182446@gmail.com>
2026-03-29 20:51:42 -0300

    feat(questao-2): Adicionando a questao 2

---
+++ questao-2/Date.java
@@ -0,0 +1,66 @@
+public class Date {
+    // instanciandos objetos
+    private int day;
+    private int month;
+    private int year;
+
+    // construtor
+
+    public Date(int day, int month, int year){
+        // validação do dia
+        if (day >= 1 && day <= 31){
+            this.day = day;
+        }
+
+        // validação do mes
+        if (month >= 1 && month <= 12){
+            this.month = month;
+        }
+
+        // validação de ano
+
+        if (year > 0){
+            this.year = year;
+        }
+
+    }
+
+     // metodos set
+        // geralmente não devolvem valor e apenas alteram o resultado, por isso é usado o void em vez de int
+        public void setDay(int day){
+            if (day >= 1 && day <= 31){
+            this.day = day;
+        }
+    }
+
+    public void setMonth(int month){
+            if (month >= 1 && month <= 12){
+            this.month = month;
+        }
+    }
+
+    public void setYear(int year){
+        if (year > 0){
+            this.year = year;
+        }
+    }
+
+    // metodos get
+    public int getDay(){
+        return day;
+    }
+
+    public int getMonth(){
+        return month;
+    }
+
+    public int getyear(){
+        return year;
+    }
+
+    public void displayDate() {
+    // Aqui usamos o printf para formatar a data com as barras /
+    System.out.printf("%d/%d/%d%n", day, month, year);
+}
+
+}
---
+++ questao-2/DateTest.java
@@ -0,0 +1,13 @@
+public class DateTest {
+
+    public static void main(String[] args) {
+
+        // Criando o objeto (o molde)
+        Date minhaData = new Date(5, 11, 2005);
+
+        System.out.print("A data selecionada é: ");
+        minhaData.displayDate();
+
+    }
+
+}
